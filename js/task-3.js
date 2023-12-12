ca80f83a8c200880021eccc03308ba8cc83b15be
seller07k <138016173+seller07k@users.noreply.github.com>
2023-12-12 16:57:17 +0200

    я

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function calculateEvenSum(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+console.log(calculateEvenSum(6));
