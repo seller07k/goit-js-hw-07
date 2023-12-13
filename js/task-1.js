084c6c31e7a15495cb37e567323589b7af728e0e
seller07k <138016173+seller07k@users.noreply.github.com>
2023-12-13 11:10:41 +0200

    !

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,15 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function makeTransaction(quantity, pricePerDroid, customersCredit) {
+  let message;
+  let totalPrice = quantity * pricePerDroid;
+  if (totalPrice > customersCredit) {
+    message = "Insufficient funds!";
+  } else {
+    message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
   return message;
 }
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000, 23000));
+console.log(makeTransaction(3, 1000, 15000));
+console.log(makeTransaction(10, 5000, 8000));
+console.log(makeTransaction(8, 2000, 10000));
+console.log(makeTransaction(10, 500, 5000));
--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,18 @@
-function calculateEvenSum(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    if (i % 2 === 0) {
-      sum += i;
-    }
+function checkForSpam(message) {
+  const checkForText = message.toLowerCase();
+  const checkForBig = message.toUpperCase();
+  if (checkForText.includes("sale", "spam", "not a spam")) {
+    return true;
+  } else if (checkForBig.includes("SPAM")) {
+    return true;
+  } else {
+    return false;
   }
-
-  return sum;
 }
-console.log(calculateEvenSum(6));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
