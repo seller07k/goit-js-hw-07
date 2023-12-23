0280f9f936f347d4f18bd6ebc626aa31cffe4902
seller07k <138016173+seller07k@users.noreply.github.com>
2023-12-23 18:33:37 +0200

    11

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,8 @@
-function makeTransaction(quantity, pricePerDroid, customersCredit) {
-  let message;
-  let totalPrice = quantity * pricePerDroid;
-  if (totalPrice > customersCredit) {
-    message = "Insufficient funds!";
-  } else {
-    message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  }
-  return message;
+function slugify(title) {
+  const fixedTitle = title.toLowerCase().split(" ").join("-");
+  return fixedTitle;
 }
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,14 @@
-function checkForSpam(message) {
-  const checkForText = message.toLowerCase();
-  const checkForBig = message.toUpperCase();
-  if (checkForText.includes("sale", "spam", "not a spam")) {
-    return true;
-  } else if (checkForBig.includes("SPAM")) {
-    return true;
-  } else {
-    return false;
+function filterArray(numbers, value) {
+  const newArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
   }
+  return newArray;
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
