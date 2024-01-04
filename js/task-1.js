0335803f8caac916c3df19bdc439586c882bba91
seller07k <138016173+seller07k@users.noreply.github.com>
2024-01-04 18:44:03 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,12 @@
-function slugify(title) {
-  const fixedTitle = title.toLowerCase().split(" ").join("-");
-  return fixedTitle;
+function isEnoughCapacity(products, containerSize) {
+  const totalProducts = Object.values(products).reduce(
+    (acc, quantity) => acc + quantity,
+    0
+  );
+
+  return totalProducts <= containerSize;
 }
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
